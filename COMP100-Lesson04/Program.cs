fed9f323900975e9ac5215b84ac9e224d2bf3530
tsiliopoulos <tsiliopoulos@hotmail.com>
2015-01-28 21:44:42 -0500

    Demonstrated Data Manipulation with this project

--- COMP100-Lesson04/Program.cs
+++ COMP100-Lesson04/Program.cs
@@ -8,13 +8,28 @@ namespace COMP100_Lesson04
 {
     class Program
     {
-        static void Main(string[] args)
+
+        static void Main(string[] args) // This is our first Program
         {
-            Console.WriteLine("Hello World!");
+            int myValue = 101;
+
+            Console.WriteLine("My Current Value is: {0}", myValue);
+            Console.WriteLine();
+            Console.Write("Enter any value: ");
+            string newValue = Console.ReadLine();
+            Console.WriteLine("You Typed: {0}", newValue);
+            Console.WriteLine("I'm adding it to my current value");
+            myValue += Convert.ToInt32(newValue);
+            Console.WriteLine("++++++++++++++++++++++++++++++++");
+            Console.WriteLine();
+
+            Console.WriteLine("My New Value is: {0}", myValue);
+
             Console.WriteLine();
             Console.WriteLine("++++++++++++++++++++++++++++++++");
             Console.WriteLine("Press any key to Exit...");
             Console.ReadKey();
+             
         }
     }
 }
